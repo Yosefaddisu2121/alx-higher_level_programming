b33b946ceb0c94134747e4b69ce0c0df6d71d4d5
Yosefaddisu2121 <yosefaddisu777@gmail.com>
2024-04-14 23:53:35 -0700

    hoping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python3
-"""Module that lists all states from mysql database"""
-import sys
+"""
+Lists all states from the database hbtn_0e_0_usa sorted in ascending order by
+states.id
+"""
 import MySQLdb
-def list_states(username, password, database):
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host='localhost',port=3306, user=username,passwd=password, db=database)
-    cursor = db.cursor()
-    #Execute the SQL query to fetch all states
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    # Fetch all the rows from the quert result.
-    rows = cursor.fetchall()
-    #Print the results
+import sys
+
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    try:
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=db_name,
+            charset="utf8"
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to database: {}".format(e))
+        sys.exit(1)
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+
     for row in rows:
         print(row)
-    #clone the database connection
-    db.close()
-#Example usage
-if __name__== '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
 
-    list_states(username, password, database)
+    cur.close()
+    conn.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-"""Module thet lists all states from the hbtn_0e_0_usa database."""
-import sys
-import MySQLdb
+"""
+This script lists all cities from
+the database `hbtn_0e_4_usa`.
+"""
 
-if __name__=="__main__":
-    #Get MySQL credentials and search name from command-line arguments
-    db = MySQLdb.connect(user=sys.argv[1],passwd=sys.argv[2], db=sys.argv[3]
-    c = db.cursor()
+import MySQLdb as db
+from sys import argv
 
-    #Execute the SQL query to retrieve states with the specified name
-    c.execute("SELECT `c`.`id`. `c`.`name`, `n`.`name` \
-            FROM `cities` as `c` \
-            INNER JOIN `states` as `s` \
-            ON `c`.`states_id` == `s`.`id` \
-            ORDER BY `c`.`id`")
-    # Fetch all rows and print the states 
-    [print(city) for city in c.fetchall()]
+if __name__ == '__main__':
+    """
+    Access the database and get the cities
+    from the database.
+    """
+
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+
+    with db_connect.cursor() as db_cursor:
+        db_cursor.execute("SELECT cities.id, cities.name, states.name \
+                                FROM cities JOIN states ON cities.state_id \
+                                = states.id ORDER BY cities.id ASC")
+        rows_selected = db_cursor.fetchall()
+
+    if rows_selected is not None:
+        for row in rows_selected:
+            print(row)
