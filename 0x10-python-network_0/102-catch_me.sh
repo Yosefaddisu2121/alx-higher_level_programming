da5e9cd0d4d07c11310b9be4dd9fef1542907a62
Yosefaddisu2121 <yosefaddisu777@gmail.com>
2024-04-25 13:13:10 -0700

    zxcv

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a PUT request with a custom header to the specified URL
-curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+# Script to make a request to 0.0.0.0:5000/catch_me and display the response
+curl -s -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
