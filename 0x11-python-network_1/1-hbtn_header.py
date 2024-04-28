0b207053640bf5cf421050d17a3ef65ad80a80dc
Yosefaddisu2121 <yosefaddisu777@gmail.com>
2024-04-28 03:36:41 -0700

    asdfg

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""
-Sends a request to a URL and displays the value of the X-Request-Id variable found in the header of the response.
-"""
 import urllib.request
 import sys
 if __name__ == "__main__":
