9cf98ed9ffff53205758f419b2a95228f045198e
Yosefaddisu2121 <yosefaddisu777@gmail.com>
2024-04-28 04:21:00 -0700

    salvage

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,10 +7,14 @@ import urllib.request
 import urllib.parse
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-
+def send_post_request(url, email):
+    """
+    Sends a POST request to the specified URL with the provided email parameter.
+    
+    Args:
+        url (str): The URL to send the request to.
+        email (str): The email parameter to include in the request.
+    """
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
     req = urllib.request.Request(url, data=data, method='POST')
@@ -19,4 +23,12 @@ if __name__ == "__main__":
         body = response.read().decode('utf-8')
         print(body)
 
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <URL> <email>")
+        sys.exit(1)
+    
+    url = sys.argv.get(1)
+    email = sys.argv.get(2)
+    send_post_request(url, email)
 
