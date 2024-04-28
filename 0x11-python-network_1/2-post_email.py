f67827675fcb578f37b296d2a87762327ffa27c5
Yosefaddisu2121 <yosefaddisu777@gmail.com>
2024-04-28 04:13:41 -0700

    salvage

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,8 +8,8 @@ import urllib.parse
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv.get(1)
-    email = sys.argv.get(2)
+    url = sys.argv[1]
+    email = sys.argv[2]
 
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
