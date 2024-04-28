4d40db74b7e11b16141747c8e0561389076d4960
Yosefaddisu2121 <yosefaddisu777@gmail.com>
2024-04-28 04:05:09 -0700

    salvage

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,10 +4,10 @@ fetch the program
 """
 import urllib.request
 import sys
+
 if __name__ == "__main__":
-    url = sys.argv[1]
+    url = sys.argv.get(1)
     with urllib.request.urlopen(url) as response:
         x_request_id = response.getheader('X-Request-Id')
         print(x_request_id)
-
 
